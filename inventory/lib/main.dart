adb77271d3ea79121036fb9ac513227e532bade9
Dago Tellez <dtellezgar@gmail.com>
2024-04-02 15:40:09 -0400

    fix error

--- inventory/lib/main.dart
+++ inventory/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:inventory/qrScanner.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Inventory'),
+      home: const QRScanner(),
     );
   }
 }
--- inventory/lib/resultScreen.dart
+++ inventory/lib/resultScreen.dart
@@ -1,31 +1,31 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:inventory/qrScanner.dart';
 import 'package:qr_flutter/qr_flutter.dart';
-
 
 class ResultScreen extends StatelessWidget {
   final String code;
   final Function() closeScreen;
 
-  const ResultScreen({Key? key, required this.closeScreen, required this.code})
-      : super(key: key);
+  const ResultScreen({
+    Key? key,
+    required this.closeScreen,
+    required this.code,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: bgColor,
+      backgroundColor: Colors.white, // Change bgColor to Colors.white
       appBar: AppBar(
         leading: IconButton(
-            onPressed: () {
-              closeScreen();
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back_ios_new_rounded,
-              color: Colors.black87,
-            )),
+          onPressed: () {
+            closeScreen();
+            Navigator.pop(context);
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new_rounded,
+            color: Colors.black87,
+          ),
+        ),
         centerTitle: true,
         title: const Text(
           "QR Scanner",
@@ -36,21 +36,19 @@ class ResultScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(
-        alignment: Alignment.center,
+      body: Center(
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //muestra el codigo Qr escaneado
               QrImageView(
                 data: code,
-                size: 150,
                 version: QrVersions.auto,
+                size: 150,
               ),
-
-
+            ],
+          ),
         ),
       ),
     );
